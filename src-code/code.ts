205e6855c72049c027dcd29f6f286851ceddc4d3
Zuri Klaschka <pklaschka@users.noreply.github.com>
2025-02-03 18:07:41 +0100

    feat: Implement text node layout type checking workaround
    Note that this might break in the future and should definitely be replaced by `TextNode.layout` once that's no longer experimental, but it seems to work for most cases ... for now.

--- src-code/code.ts
+++ src-code/code.ts
@@ -1,33 +1,79 @@
 import addOnSandboxSdk from "add-on-sdk-document-sandbox";
 
-import { editor, colorUtils } from "express-document-sdk";
+import { editor, TextNode, Node, constants } from "express-document-sdk";
 
 const { runtime } = addOnSandboxSdk.instance;
 
 console.log("Sandbox code is running nowww");
 
 const sandboxApi = {
-  createBox: (width: number, height: number) => {
-    const insertionParent = editor.context.insertionParent;
-    const rectangle = editor.createRectangle();
-    rectangle.width = width;
-    rectangle.height = height;
-    rectangle.translation = { x: 100, y: 20 };
-    const rectFill = editor.makeColorFill(
-      colorUtils.fromRGB(
-        Math.random(),
-        Math.random(),
-        Math.random(),
-        Math.random(),
-      ),
-    );
-    rectangle.fill = rectFill;
-    insertionParent.children.append(rectangle);
-    return true;
+  determineTextNodeType() {
+    const selection = editor.context.selection.filter(isTextNode);
+    if (selection.length === 0) {
+      return "No text node selected";
+    }
+    return `Text node type: ${
+      isLikePointText(selection[0]) ? "Point Text" : "Auto Height Text"
+    }`;
   },
 };
 
 export type SandboxRemoteType = typeof sandboxApi;
+
+/**
+ * Determines if the passed node is a `TextNode`
+ * @param node
+ * @returns `true` if the node is a `TextNode`, `false` otherwise
+ */
+function isTextNode(node: Node): node is TextNode {
+  return node.type === constants.SceneNodeType.text;
+}
+
+/**
+ * Checks if a given text node is like a point text.
+ * This is the case if the height of the text node does not increase with text.
+ * In this case, the amount of placeholder text cannot be determined by the height of the text node.
+ * @param textNode a text node to check
+ * @returns `true` if the text node is like a point text, `false` otherwise
+ */
+function isLikePointText(textNode: TextNode) {
+  const prevHeight = textNode.boundsInParent.height;
+  const prevText = textNode.fullContent.text;
+
+  textNode.fullContent.text = "fjo23qj9o23jro23jfo23jfo23jfo23f".repeat(10);
+
+  const newHeight = textNode.boundsInParent.height;
+
+  let changedWidthAtSomePoint = doesWidthChangeWithText(textNode);
+
+  textNode.fullContent.text = prevText;
+
+  return (changedWidthAtSomePoint || prevHeight === newHeight);
+}
+
+/**
+ * Checks if the width of a text node changes with text. This is the case for:
+ * - Point Text
+ * - Dynamic Text (but only sometimes / for specific combinations of text)
+ * - "Text Layouts" (circle, arch, bow)
+ *
+ * In these cases, we cannot rely on the height of the text node to monotonically increase with text.
+ *
+ * @param textNode text node to check
+ * @param maxIterations iterations to check with (needed for dynamic layouts that only change sometimes)
+ * @returns `true` if the width changes with text, `false` otherwise
+ */
+function doesWidthChangeWithText(textNode: TextNode, maxIterations = 10) {
+  const prevWidth = textNode.boundsInParent.width;
+
+  for (let i = 0; i < maxIterations; i++) {
+    textNode.fullContent.text += " fjo23qj9o23jro23jfo23 jfo23jfo23f";
+    if (textNode.boundsInParent.width !== prevWidth) {
+      return true;
+    }
+  }
+  return false;
+}
 
 async function start() {
   runtime.exposeApi(sandboxApi);
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { sandbox } from "./utils/utils";
+import { getRuntime } from "./utils/utils";
 
 
 export const App = () => {
+  const [textType, setTextType] = useState("TBD");
+  async function getTextType() {
+    const runtime = await getRuntime();
+    setTextType(await runtime.determineTextNodeType());
+    // setTextType("Hello World");
+  }
   
   useEffect(() => {}, []);
   return (
     <>
       <main>
         <h1>Hello Lorem Ipsum!</h1>
-        
+        <p>{textType}</p>
+        <button onClick={getTextType}>Click me</button>
       </main>
     </>
   );
